9fbfd4713dcc1cd24f8395796e009976a4d8a310
Paul Christmann <pchristmann@Pauls-MacBook-Pro.local>
2013-12-04 20:46:48 -0600

    As usual, brute force first.

--- ruby/lib/project_euler/problems/problem_9.rb
+++ ruby/lib/project_euler/problems/problem_9.rb
@@ -3,6 +3,20 @@ module ProjectEuler
     class Problem9
       def initialize
       end
+      # a+b+c = sum and a^2 + b^2 = c^2
+      # return a*b*c
+      def pythagorean_product(sum)
+        (1..sum).each do |a|
+          (a..sum).each do |b|
+            (b..sum).each do |c|
+              next unless a+b+c == sum
+              next unless a*a + b*b == c*c
+              return a*b*c
+            end
+          end
+        end
+        0
+      end
     end
   end
 end
--- ruby/spec/unit/problems/problem_9_spec.rb
+++ ruby/spec/unit/problems/problem_9_spec.rb
@@ -5,6 +5,11 @@ describe Problem9 do
   describe 'problem 9' do
     it 'should validate baseline' do
       p = Problem9.new
+      p.pythagorean_product(12).should == 60
+    end
+    it 'should solve' do
+      p = Problem9.new
+      p.pythagorean_product(1000).should == 31875000
     end
   end
 end
