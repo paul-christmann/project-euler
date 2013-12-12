d0f5ed730d268c134c67d8f6b3d200597bac4a39
Paul Christmann <pchristmann@Pauls-MacBook-Pro.local>
2013-12-11 23:12:36 -0600

    Finished up date calculations

--- ruby/lib/project_euler/problems/problem_19.rb
+++ ruby/lib/project_euler/problems/problem_19.rb
@@ -7,7 +7,81 @@ module ProjectEuler
       end
       
       def num_sundays
+        result = 0
+        day = JANUARY_1_1900
+        while day.year <= @end_year
+          if day.year >= @start_year
+            if day.is_sunday? && day.is_first_of_month?
+              result +=1 
+            end
+          end
+          day = day.next
+        end
+        result
       end
     end
+    
+    class Date19
+      attr_accessor :year, :month, :dow, :dom, :day
+      def initialize(attrs)
+        self.year = attrs[:year]
+        self.month = attrs[:month]
+        self.dow = attrs[:dow] % 7
+        self.dom = attrs[:dom] 
+        self.day = attrs[:day]
+      end
+      
+      def is_sunday?
+        self.dow == 0
+      end
+      
+      def is_first_of_month?
+        self.dom == 1
+      end
+      
+      def days_in_month
+        if [1,3,5,7,8,10,12].include?(self.month) 
+          31 
+        elsif [4,6,9,11].include?(self.month) 
+          30
+        else
+          if self.year % 400 == 0
+            29
+          elsif self.year % 100 == 0
+            28
+          elsif self.year % 4 == 0
+            29
+          else
+            28
+          end
+        end
+      end
+      
+      def next
+        next_year = self.year
+        next_day = self.day + 1
+        
+        next_dow = (self.dow + 1) % 7
+        
+        if self.dom + 1 > days_in_month
+          next_dom = 1
+          next_month = self.month + 1
+          
+          if next_month > 12
+            next_month = 1
+            next_year = year + 1
+          end
+          
+        else
+          next_dom = self.dom + 1
+          next_month = self.month
+        end
+        
+        Problem19::Date19.new({:year => next_year, :month => next_month, :dow => next_dow, :dom => next_dom, :day => next_day})
+      end
+    end
+    
+    JANUARY_1_1900 = ProjectEuler::Problems::Problem19::Date19.new({:day => 0, :year => 1900, :month => 1, :dow => 1, :dom => 1})
+    
   end
 end
--- ruby/spec/unit/problems/problem_19_spec.rb
+++ ruby/spec/unit/problems/problem_19_spec.rb
@@ -4,9 +4,89 @@ include ProjectEuler::Problems
 describe Problem19 do
   describe 'problem 19' do
     
-    it 'should solve baseline' do
-      p = Problem19.new(1901, 1901)
-      p.num_sundays.should == 3
+    it 'should solve problem' do
+      p = Problem19.new(2013, 2013)
+      p.num_sundays.should == 2
     end
+
+    it 'should solve problem' do
+      p = Problem19.new(1901, 2000)
+      p.num_sundays.should == 171
+    end
+    
+    it 'should solve baseline' do
+      d = Problem19::JANUARY_1_1900
+      d.day.should == 0
+      d.dow.should == 1
+      d.is_first_of_month?.should == true
+      d.is_sunday?.should == false
+      d.days_in_month.should == 31
+      d.next.next.next.next.next.next.is_sunday?.should == true
+    end
+    
+    it 'should solve one leap year' do
+      d = Problem19::JANUARY_1_1900
+      while d.year != 1904 
+        d = d.next
+      end
+      d.year.should == 1904
+      d.dom.should == 1
+      d.month.should == 1
+      (1..31).each do
+        d = d.next
+      end
+      d.year.should == 1904
+      d.dom.should == 1
+      d.month.should == 2
+      (1..28).each do
+        d = d.next
+      end
+      d.year.should == 1904
+      d.dom.should == 29
+      d.month.should == 2
+
+      d = d.next
+      d.dom.should == 1
+      d.month.should == 3
+    end
+    it 'should solve one non-leap year' do
+      d = Problem19::JANUARY_1_1900
+      (1..30).each do
+        d = d.next
+      end
+      d.dom.should == 31
+      d.month.should == 1
+      d.year.should == 1900
+      
+      d = d.next
+      d.dom.should == 1
+      d.month.should == 2
+      d.year.should == 1900
+      (1..27).each do 
+        d = d.next
+      end
+      d.dom.should == 28
+      d.month.should == 2
+      d.year.should == 1900
+      
+      d = d.next
+      d.dom.should == 1
+      d.month.should == 3
+      d.year.should == 1900
+      
+      d = Problem19::JANUARY_1_1900
+      (1..364).each do
+        d = d.next
+      end
+      d.dom.should == 31
+      d.month.should == 12
+      d.year.should == 1900
+      
+      d = d.next
+      d.dom.should == 1
+      d.month.should == 1
+      d.year.should == 1901
+    end
+
   end
 end
